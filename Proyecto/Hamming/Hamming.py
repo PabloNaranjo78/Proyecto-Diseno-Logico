7a7d531bf57a6a8db5498a298839cc2d22bfef32
Joel Gómez <62965696+Joel-Araya@users.noreply.github.com>
2022-09-05 06:09:04 -0600

    Corrección de error

--- Proyecto/Hamming/Hamming.py
+++ Proyecto/Hamming/Hamming.py
@@ -3,69 +3,41 @@
 
 class Hamming:
 
+	correctHamming=''
+
 	def start(self, data):
 
-		# Calculate the no of Redundant Bits Required
 		m = len(data)
 		r = self.calcRedundantBits(m)
 
-		# Determine the positions of Redundant Bits
 		arr = self.posRedundantBits(data, r)
-		print("posRedundantBits es", arr)
-
-		# Determine the parity bits
 		arr = self.calcParityBits(arr, r)
 
-		# Data to be transferred
 		print("Data transferred (calcParityBits) is ", arr)
 
-		# Stimulate error in transmission by changing
-		# a bit value.
-		# 10101001110 -> 11101001110, error in 10th position.
-
-		# arr = '00011111010011011'
-		aux='00011111010011011'
-
-
-		# print("Error Data is " + arr)
-		# correction = self.detectError(arr, r)
-		# if (correction == 0):
-		# 	print("There is no error in the received message.")
-		# else:
-		# 	print("The position of error is ", len(arr) - correction + 1, "from the left")
-
+		self.correctHamming=arr
 		return arr
 
 	def calcRedundantBits(self, m):
-
-		# Use the formula 2 ^ r >= m + r + 1
-		# to calculate the no of redundant bits.
-		# Iterate over 0 .. m and return the value
-		# that satisfies the equation
 
 		for i in range(m):
 			if(2**i >= m + i + 1):
 				return i
 
 	def posRedundantBits(self, data, r):
-
-		# Redundancy bits are placed at the positions
-		# which correspond to the power of 2.
 		j = 0
 		k = 1
 		m = len(data)
 		res = ''
-
-		# If position is power of 2 then insert '0'
-		# Else append the data
-		for i in range(1, m + r+1):
+		data=data[::-1]
+		for i in range(1, m+r+1):
 			if(i == 2**j):
 				res = res + 'p'
 				j += 1
 			else:
 				res = res + data[-1 * k]
 				k += 1
-
+		print(res)
 		return res
 
 
@@ -76,7 +48,6 @@ class Hamming:
 		movementsList=[]
 		movementsList.append(arr)
 
-		# print("=============\n")
 		for i in range(r):
 			par=2**i
 			auxPar = self.getBinary(par).find('1')
@@ -104,26 +75,6 @@ class Hamming:
 		return movementsList
 
 
-	def detectError(self, arr, nr):
-		n = len(arr)
-		res = 0
-
-		# Calculate parity bits again
-		for i in range(nr):
-			val = 0
-			for j in range(1, n + 1):
-				if(j & (2**i) == (2**i)):
-					val = val ^ int(arr[-1 * j])
-
-			# Create a binary no by appending
-			# parity bits together.
-
-			res = res + val*(10**i)
-
-		# Convert binary to decimal
-		return int(str(res), 2)
-
-
 	def getBinary(self, num):
 		aux = bin(num)[2::]
 
@@ -144,9 +95,64 @@ class Hamming:
 
 
 
+
+	def detectError(self, newArr):
+
+		# n = len(arr)
+		# res = 0
+		#
+		# # Calculate parity bits again
+		# for i in range(nr):
+		# 	val = 0
+		# 	for j in range(1, n + 1):
+		# 		if(j & (2**i) == (2**i)):
+		# 			val = val ^ int(arr[-1 * j])
+		#
+		# 	# Create a binary no by appending
+		# 	# parity bits together.
+		#
+		# 	res = res + val*(10**i)
+
+		# Convert binary to decimal
+		return int(str(1), 2)
+
+	def calcParityBitsForError(self, newArr):
+		n = len(newArr)
+
+		auxCheck=''
+		movementsList=[]
+		movementsList.append(newArr)
+
+		for i in range(5):
+			par=2**i
+			auxPar = self.getBinary(par).find('1')
+
+			for j in range(n):
+				if self.getBinary(j+1)[auxPar]=='1':
+					auxCheck += newArr[j]
+				else:
+					auxCheck += ' '
+
+
+			posPAuxCheck = 2**i-1
+			# print("pos es", posPAuxCheck, "y arr es", arr)
+			auxCheck=self.chekParity(auxCheck)
+			# newArr = newArr.replace('p', self.correctHamming[posPAuxCheck], 1)
+			movementsList.append(auxCheck)
+			auxCheck=''
+
+
+		movementsList[0] = movementsList[0].replace('p',' ')
+		# print("Los movimientos realizados son", movementsList)
+
+		print(movementsList)
+		return movementsList
+
+
 if __name__ == "__main__":
 	hamming = Hamming()
 	hamming.start('101100101110')
+	# hamming.calcParityBitsForError('00011111010011010')
 
 
 
