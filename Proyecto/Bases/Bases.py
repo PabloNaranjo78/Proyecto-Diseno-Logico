521e6c0c994b08cd64a4671ec6d637b9c8a36927
FMonge <fmonge13@estudiantec.cr>
2022-09-04 20:14:34 -0600

    Bases python

---
+++ Proyecto/Bases/Bases.py
@@ -0,0 +1,66 @@
+class Bases:
+    def __init__(self):
+        self.length = None
+        self.decimal = None
+        self.binary = ""
+        self.number = None
+        self.hexadecimal = None
+        self.validValues = "01234567"
+        self.conversion_table = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+                                5: '5', 6: '6', 7: '7',8: '8', 9: '9',
+                                10: 'A',11: 'B', 12: 'C', 13: 'D',
+                                14: 'E', 15: 'F'}
+
+    def is_valid(self, num):
+        self.number = int(num)
+
+        if (self.composition(num) and 0 <= self.number <= 777):
+            self.length = len(str(self.number))
+
+            if self.number == 0:
+                self.decimal = 0
+                self.binary = 0
+                self.hexadecimal = "0"
+            return True
+        else:
+            return False
+
+    def composition(self, num):
+        for x in num:
+            if x not in self.validValues:
+                return False
+        return True
+
+    def to_decimal(self, aux):
+        num = int(aux)
+        count = 0
+        base = 1
+        while(num):
+            last = num % 10
+            num = int(num/10)
+            count += last * base
+            base *= 8
+        self.decimal = count
+
+    def to_binary(self, num):
+        if num > 1:
+            self.to_binary(num // 2)
+        self.binary += str(num % 2)
+
+    def to_hexadecimal(self, num):
+        hexa = ""
+        while (num > 0):
+            remainder = num % 16
+            hexa = self.conversion_table[remainder] + hexa
+            num = num // 16
+
+        self.hexadecimal = hexa
+
+    def get_decimal(self):
+        return self.decimal
+
+    def get_binary(self):
+        return self.binary
+
+    def get_hexadecimal(self):
+        return self.hexadecimal
--- Proyecto/main.py
+++ Proyecto/main.py
@@ -1,19 +1,26 @@
 import GUI.MainWindow as MainWindow
+import Bases.Bases as hp
 from tkinter import *
 
 width = 1000
 height = 600
 
 if __name__ == '__main__':
-    master = Tk()
-    master.title("Proyecto Diseño Lógico")
-    master.geometry(str(width) + "x" + str(height))
-    master.resizable(False,False)
-
-    background = PhotoImage(file="Images/Background.gif")
-    graph_img = PhotoImage(file="Images/graph.gif")
-
-    mainWindow = MainWindow.MainWindow(master=master, width=width,
-                                       height=height,background=background,
-                                       graph_img=graph_img)
-    master.mainloop()
+    # master = Tk()
+    # master.title("Proyecto Diseño Lógico")
+    # master.geometry(str(width) + "x" + str(height))
+    # master.resizable(False,False)
+    #
+    # background = PhotoImage(file="Images/Background.gif")
+    # graph_img = PhotoImage(file="Images/graph.gif")
+    #
+    # mainWindow = MainWindow.MainWindow(master=master, width=width,
+    #                                    height=height,background=background,
+    #                                    graph_img=graph_img)
+    # master.mainloop()
+    bases = hp.Bases()
+    bases.to_decimal("643")
+    bases.to_binary(bases.get_decimal())
+    bases.get_binary()
+    bases.to_hexadecimal(bases.get_decimal())
+    bases.get_hexadecimal()
